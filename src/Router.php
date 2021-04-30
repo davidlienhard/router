661da8a586d2fb35a2a0b90d86c2b24681bc65be
David Lienhard <david.lienhard@tourasia.ch>
2021-04-30 20:26:35 +0200

    update codestyle update codestyle for new standard

--- src/Router.php
+++ src/Router.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace DavidLienhard\Router;
 
-use \DavidLienhard\Router\RouterInterface;
+use DavidLienhard\Router\RouterInterface;
 
 /**
  * provides routing functionality
